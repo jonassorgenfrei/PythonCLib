5bbb43667e5485922e32bbe02d39ed77ccc94512
Jonas Sorgenfrei <jonas_sorgenfrei@yahoo.de>
2022-10-26 03:14:12 +0200

    init data

---
+++ src/python_example.cpp
@@ -0,0 +1,35 @@
+#include <boost/python.hpp>
+#include <string>
+#include <sstream>
+
+class SomeClass
+{
+public:
+    SomeClass(std::string n) : name(n), mNumber(0.0) {}
+
+    std::string name;
+
+    double getNumber() const { return mNumber; }
+    void setNumber(double n)
+    {
+        if (n > 3.141592654)
+            n = -1;
+        mNumber = n;
+    }
+
+private:
+    double mNumber;
+};
+
+
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(pyClass)
+{
+    class_<SomeClass>("SomeClass", init<std::string>())
+        .def_readwrite("name", &SomeClass::name)
+        .add_property("number", &SomeClass::getNumber, &SomeClass::setNumber)
+        ;
+
+}
